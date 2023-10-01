8cc403b25bfb2ad2385e44872c2ccf9aa00ed488
rahukettu <78539693+rahukettu@users.noreply.github.com>
2023-10-01 23:23:13 +0300

    Tehtävä5

---
+++ Tehtävä 5/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "asunto.h"
+using namespace std;
+
+
+
+int main()
+{
+    //Asunto testiasunto;
+    //Kerros testikerros;
+    //Katutaso testikatutaso;
+    Kerrostalo* testikerrostalo = new Kerrostalo;
+    //testiasunto.maarita(2,100);
+    //testiasunto.laskeKulutus(1);
+    //testikatutaso.maaritaAsunnot();   
+    //testikerros.maaritaAsunnot(); 
+    cout<<"Kerrostalon kulutus on "<<testikerrostalo->laskeKulutus(1)<<endl;
+
+    return 0;
+}
